f585e3ffead6357d6317c7399ecd29e6dae1f462
ksharipov <firskov@mail.ru>
2019-08-16 17:28:28 +0300

    clear code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,6 @@ import {
 import { FormControl } from '@angular/forms';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { MatIconRegistry } from '@angular/material/icon';
-import { stripGeneratedFileSuffix } from '@angular/compiler/src/aot/util';
-import { ChangeDetection } from '@angular/cli/lib/config/schema';
 
 
 export interface PeriodicElement {
